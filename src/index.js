809367c6d56293253ef9d46a5f6438effec96bf1
Lior <levydlior@gmail.com>
2022-05-23 15:37:16 -0600

    sd

--- src/index.js
+++ src/index.js
@@ -1,3 +1,21 @@
 document.addEventListener("DOMContentLoaded", () => {
-  // your code here
-});
+  const newTaskForm = document.getElementById("create-task-form");
+
+  newTaskForm.addEventListener("submit", e => makeShitHappen(e));
+
+
+function makeShitHappen(e){
+  e.preventDefault();
+  //declare const to hold user input value
+  const textBox = document.getElementById("new-task-description");
+  
+  //append a list item to an unorded list
+  //add a button to each task list
+  const taskList = document.getElementById("tasks");
+  const newTask = document.createElement("li");
+  newTask.textContent = textBox.value;
+  taskList.append(newTask);
+  newTaskForm.reset();
+}
+}
+);
